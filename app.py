9d39cd4ff2c7427fefdb417327e0a52c2f523043
natebachmeier <nate@bachmeier>
2021-03-26 00:32:07 -0400

    vpn-connection

--- app.py
+++ app.py
@@ -2,14 +2,14 @@
 import os.path
 from aws_cdk.core import App, Stack, Environment, Construct, NestedStack
 from infra.exports import create_layers, landing_zone
-from infra.networking import VpcPeeringConnection
+from infra.networking import VpcPeeringConnection, HomeNetVpn
 src_root_dir = os.path.join(os.path.dirname(__file__))
 
 us_east_1 = Environment(region="us-east-1", account='581361757134')
 eu_west_1 = Environment(region="eu-west-1", account='581361757134')
 ap_ne_1 = Environment(region='ap-northeast-1', account='581361757134')
 
-vpc_ids = {1
+vpc_ids = {
   'ireland':'vpc-0e953932de63095c4',
   'tokyo':'vpc-0280a30d9b234a71e'
 }
@@ -52,5 +52,8 @@ def create_peering():
     peer_vpc_id=vpc_ids['tokyo'],#ireland.vpc.vpc_id,
     peer_region=ap_ne_1.region)
 
+#create_peering()
+
+#vpn = HomeNetVpn(virginia, 'Vpn',virginia.vpc)
 
 app.synth()
--- infra/networking.py
+++ infra/networking.py
@@ -21,11 +21,12 @@ class NetworkingLayer(core.Construct):
       enable_dns_hostnames=True,
       enable_dns_support=True,
       max_azs=2,
-      nat_gateways=0,
+      nat_gateways=1,
       subnet_configuration=[
         ec2.SubnetConfiguration(name='NetStore', subnet_type= ec2.SubnetType.ISOLATED, cidr_mask=24),
-        ec2.SubnetConfiguration(name='Identity', subnet_type= ec2.SubnetType.ISOLATED, cidr_mask=27)
-        #ec2.SubnetConfiguration(name='Public', subnet_type=ec2.SubnetType.PUBLIC, cidr_mask=27),
+        ec2.SubnetConfiguration(name='Identity', subnet_type= ec2.SubnetType.ISOLATED, cidr_mask=27),
+        ec2.SubnetConfiguration(name='Vpn', subnet_type=ec2.SubnetType.PRIVATE, cidr_mask=27),
+        ec2.SubnetConfiguration(name='Public', subnet_type=ec2.SubnetType.PUBLIC, cidr_mask=28),
         #ec2.SubnetConfiguration(name='NCU', subnet_type= ec2.SubnetType.PRIVATE, cidr_mask=24)
       ])
     VpcEndpointsForAWSServices(self,'Endpoints',vpc=self.vpc)
@@ -37,7 +38,41 @@ class VpcPeeringConnection(core.Construct):
   def __init__(self, scope: core.Construct, id: str, vpc:ec2.IVpc, peer_vpc_id:str,peer_region:str, **kwargs) -> None:
     super().__init__(scope, id, **kwargs)
 
-    self.peering = ec2.CfnVPCPeeringConnection(scope,'Peer',
+    self.peering = ec2.CfnVPCPeeringConnection(scope,'Peer/'+id,
       peer_region=peer_region,# core.Stack.of(peer).region,
       peer_vpc_id= peer_vpc_id,# peer.vpc_id,
       vpc_id=vpc.vpc_id)
+
+class HomeNetVpn(core.Construct):
+  """
+  Establish the vpn connection
+  """
+  def __init__(self, scope: core.Construct, id: str,vpc:ec2.IVpc, **kwargs) -> None:
+    super().__init__(scope, id, **kwargs)
+
+    customer_gateway = ec2.CfnCustomerGateway(self,'CustomerGateway',
+      ip_address='173.54.122.113',
+      bgp_asn=65000,
+      type='ipsec.1',
+      tags=[core.CfnTag(key='Name',value='TP-Link Vpn Router')])
+
+    vpn_gateway = ec2.CfnVPNGateway(self,'VpnGateway',
+      amazon_side_asn=64512,
+      type='ipsec.1',
+      tags=[core.CfnTag(key='Name',value='HomeNetGateway')])
+
+    ec2.CfnVPCGatewayAttachment(self,'HomeNetGatewayAttachment',
+      vpc_id=vpc.vpc_id,
+      vpn_gateway_id=vpn_gateway.ref)
+
+    # [net.route_table.id for net in vpc.select_subnets(subnet_group_name='Vpn').subnets]
+    routes = ec2.CfnVPNGatewayRoutePropagation(self,'VpnGatewayRouteProp',
+      route_table_ids=['rtb-08ca4caec9e6fcc65','rtb-0112514ba8d55834c'],
+      vpn_gateway_id= vpn_gateway.ref)
+    routes.add_depends_on(vpn_gateway)
+    
+    ec2.CfnVPNConnection(self,'Site2Site',
+      customer_gateway_id=customer_gateway.ref,
+      static_routes_only=True,
+      type='ipsec.1',
+      vpn_gateway_id= vpn_gateway.ref)
