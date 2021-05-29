4a85fb63379ed4b00d25cdc898f2e2c410b36e52
natebachmeier <nate@bachmeier>
2021-05-29 19:52:41 -0400

    start adding basic frontend

--- src/photo-api/handler.py
+++ src/photo-api/handler.py
@@ -4,9 +4,12 @@ from json import dumps
 from botocore.exceptions import ValidationError
 from botocore.retries import bucket
 from flask import request, redirect
+from flask.templating import render_template
+from flask.wrappers import Response
+from werkzeug.wrappers import response
 
 dynamodb = boto3.client('dynamodb')
-table_name = environ.get('TABLE_NAME')
+face_table_name = environ.get('FACE_TABLE')
 
 def init_flask_for_env():
   """
@@ -26,6 +29,46 @@ app = init_flask_for_env()
 def hello_world():
   return 'Hello, World!'
 
+@app.route('/')
+def homepage():
+  return render_template('index.html')
+
+@app.route('/css/<path:path>')
+def get_css(path:str):
+  with open('templates/css/{}'.format(path),'r') as f:
+    return Response(f.read(),mimetype='text/css')
+
+@app.route('/identities')
+def get_identities():
+  response = dynamodb.query(
+    TableName=face_table_name,
+    Select='ALL_ATTRIBUTES',
+    Limit=1000,
+    ReturnConsumedCapacity='NONE',
+    KeyConditionExpression="PartitionKey= :partitionKey",
+    ExpressionAttributeValues={
+      ":partitionKey": {'S': 'Identity'},
+    })
+  return {
+    'Identities': [x['SortKey']['S'] for x in response['Items']]
+  }
+
+@app.route('/known-faces')
+def get_known_faces():
+  response = dynamodb.query(
+    TableName=face_table_name,
+    Select='ALL_ATTRIBUTES',
+    Limit=1000,
+    ReturnConsumedCapacity='NONE',
+    KeyConditionExpression="PartitionKey= :partitionKey",
+    ExpressionAttributeValues={
+      ":partitionKey": {'S': 'KnownFaceId'},
+    })
+
+  return {
+    'KnownFaces': [x['SortKey']['S'] for x in response['Items']]
+  }
+
 @app.route('/associate/<identity>/<face_id>')
 def associate_faceid(identity:str, face_id:str):
   if identity == None:
@@ -34,11 +77,18 @@ def associate_faceid(identity:str, face_id:str):
     raise ValidationError('face_id')
   
   dynamodb.put_item(
-    TableName=table_name,
+    TableName=face_table_name,
     Item={
       'PartitionKey': {'S': 'Identity'},
-      'SortKey': {'S': identity},
-      'FaceId': {'S': face_id},
+      'SortKey': {'S': identity.lower()},
+      'display_text': {'S': identity},
+    })
+
+  dynamodb.put_item(
+    TableName=face_table_name,
+    Item={
+      'PartitionKey': {'S': 'Identity::'+identity.lower()},
+      'SortKey': {'S': face_id.lower()},
     })
 
 if __name__ == '__main__':
--- src/rtsp-update-facetable/lib/facetable.py
+++ src/rtsp-update-facetable/lib/facetable.py
@@ -2,6 +2,8 @@ from typing import Any, List, Mapping
 import boto3
 from lib.message import Message
 
+bound_box_precision = 4
+
 class FaceTable:
   def __init__(self, table_name:str, region_name:str) -> None:
     self.dynamodb = boto3.client('dynamodb', region_name=region_name)
@@ -15,22 +17,34 @@ class FaceTable:
         TableName=self.table_name,
         Item={
           'PartitionKey': {'S':'KnownFaceId'},
-          'SortKey': {'S': message.face_id}
+          'SortKey': {'S': message.face_id.lower()}
         })
       print(response)
     except Exception as error:
-      print(error)
+      print(str(error))
+      raise error
 
     # Record the Facial Impression
     try:
       response = self.dynamodb.put_item(
         TableName=self.table_name,
         Item={
-          'PartitionKey': {'S': 'FaceId:'+message.face_id},
+          'PartitionKey': {'S': 'FaceId:'+message.face_id.lower()},
           'SortKey': {'S': message.time_stamp},
-          's3_uri': {'S': message.s3_uri},
-          'camera_name': {'S': message.camera_name },
+          's3_uri': {'S': message.s3_uri },
+          'camera_name': {'S': message.camera_name.lower() },
+          'base_name': {'S': message.base_name.lower() },
+          'bounding_box': {'M': 
+            {
+              'top': { 'N': str(round(message.bounding_box['Top'],bound_box_precision)) },
+              'left': { 'N': str(round(message.bounding_box['Left'],bound_box_precision)) },
+              'height': { 'N': str(round(message.bounding_box['Height'],bound_box_precision)) },
+              'height': { 'N': str(round(message.bounding_box['Width'],bound_box_precision)) },
+            }
+          }
         })
       print(response)
     except Exception as error:
-      print(error)
+      print(str(error))
+      raise error
+
